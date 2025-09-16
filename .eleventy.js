a7cbd98d35e3f2913585eb0124d58859195d952c
Thomas Hinkle <thomashinkle2393@gmail.com>
2025-09-16 07:58:19 -0500

    updated the urls for static assets. hopefully this works on pages now.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 export default function (eleventyConfig) {
+  eleventyConfig.addFilter("url", function (url) {
+    const pathPrefix = eleventyConfig.pathPrefix || "/";
+    return `${pathPrefix}${url.replace(/^\//, "")}`;
+  });
+
   eleventyConfig.addCollection("aliens", function (collectionApi) {
     return collectionApi
       .getFilteredByGlob("aliens/*.md")
@@ -9,5 +14,6 @@ export default function (eleventyConfig) {
     dir: {
       output: "docs",
     },
+    pathPrefix: "/harmonia-archives/",
   };
 }
